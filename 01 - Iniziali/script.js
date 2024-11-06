0c0d33111eedc3459ef18ac475fe336f7eaeecd9
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-11-06 15:02:05 +0100

    es1 Iniziale (arrow function)

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -6,19 +6,29 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 //* funzione tradizionale
 
 /**
- * 
- * @param {Array} arr 
+ *
+ * @param {Array} arr
  * @returns {Array}
  */
-function firstLettersArr(arr){
-    const newArr = [];
-    for(let i=0; i<arr.length; i++){
-        const firstLetter = arr[i][0];
-        newArr.push(firstLetter);
-    }
-    return newArr;
+function firstLettersArr(arr) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const firstLetter = arr[i][0];
+    newArr.push(firstLetter);
+  }
+  return newArr;
 }
+
+const initialsArrow = (arr) => {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const firstLetter = arr[i][0];
+    newArr.push(firstLetter);
+  }
+  return newArr;
+};
 // Invoca la funzione qui e stampa il risultato in console
 const initials = firstLettersArr(names);
-console.log(initials);
+console.log("Tradizionale: " + initials);
+console.log("Arrow: " + initialsArrow(names));
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
