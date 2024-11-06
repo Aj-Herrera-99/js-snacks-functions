399b0abd09cf143394d928d7071d6838cf652b48
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-11-06 15:59:06 +0100

    es3 conta vocali (arrow function)

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -3,6 +3,7 @@
 const word = "javascript";
 
 // Dichiara la funzione qui.
+//* funzione tradizionale
 /**
  *
  * @param {string} word
@@ -12,14 +13,27 @@ function vowelCounter(word) {
   const vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
   let counter = 0;
   for (let i = 0; i < word.length; i++) {
-    if(vowels.includes(word[i])){
-        counter++;
+    if (vowels.includes(word[i])) {
+      counter++;
     }
   }
   return counter;
 }
 
+//* arrow function
+const vowelCountArrow = (word) => {
+  const vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
+  let counter = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (vowels.includes(word[i])) {
+      counter++;
+    }
+  }
+  return counter;
+};
+
 // Invoca la funzione qui e stampa il risultato in console
 const vowelCount = vowelCounter(word);
 console.log("Tradizionale: " + vowelCount);
+console.log("Arrow version: " + vowelCountArrow("BoOlEan"));
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
