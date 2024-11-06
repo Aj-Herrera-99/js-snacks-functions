c642e5647efdbc302b054d5b425a6c549a58c4a9
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-11-06 15:39:34 +0100

    es2 Filtra iniziale (funzione tradizionale)

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,35 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+//* funzione tradizionale
+/**
+ * 
+ * @param {string} letter 
+ * @param {Array} arr 
+ * @returns {Array}
+ */
+function namesStartWith(letter, arr){
+    let newArr = []; 
+    // map method per convertire il nome in minuscolo;
+    arr = arr.map(val => val.toLocaleLowerCase());
+    // converto in minuscolo anche letter
+    letter = letter.toLocaleLowerCase();
+    for(let i=0; i<arr.length; i++){
+        if(arr[i][0] === letter){
+            newArr.push(arr[i]);
+        }
+    }   
+    // map method per riconvertire in maiuscolo la prima lettera di ogni nome del nuovo array
+    newArr = newArr.map(val => {
+        val = val.replace(val[0], val[0].toLocaleUpperCase());
+        return val;
+    });
+    return newArr;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const sameInitials = namesStartWith("a", names);
+console.log("Funzione tradizionale: " + sameInitials);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
