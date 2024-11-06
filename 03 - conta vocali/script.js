11606b28a048d12909fa6c3ba20dc6fc7d302b2c
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-11-06 15:56:54 +0100

    es3 conta vocali (funzione tradizionale)

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,25 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
-
+/**
+ *
+ * @param {string} word
+ * @returns {number}
+ */
+function vowelCounter(word) {
+  const vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
+  let counter = 0;
+  for (let i = 0; i < word.length; i++) {
+    if(vowels.includes(word[i])){
+        counter++;
+    }
+  }
+  return counter;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+const vowelCount = vowelCounter(word);
+console.log("Tradizionale: " + vowelCount);
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
